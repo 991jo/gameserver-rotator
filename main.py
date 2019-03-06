8390e13776b07c692fd46307021e8b2c27638b7e
Johannes Erwerle <johannes.erwerle@googlemail.com>
2019-03-06 20:46:21 +0100

    added commandline arguments
    --help general help message
    
    --web_host IP to bind the web ui to
    --web_port tcp port to bind the web ui to

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 from threading import Thread
 
 from Rotator import Rotator
@@ -11,8 +12,22 @@ log = logging.getLogger("gameserver-rotator")
 
 log.info("initialized logging")
 
+parser = argparse.ArgumentParser(description="Gameserver Rotator")
+parser.add_argument("--web_host",
+        type=str,
+        nargs=1,
+        help="the IP the web UI is listening on, default 0.0.0.0",
+        default=["0.0.0.0"])
+parser.add_argument("--web_port",
+        type=int,
+        nargs=1,
+        help="the port the web UI is listening on, default 9000",
+        default=["9000"])
+
+args = parser.parse_args()
+
 rotator = Rotator(config)
-webui = WebUI("localhost", 9000, rotator)
+webui = WebUI(args.web_host[0], args.web_port[0], rotator)
 
 t = Thread(target=webui.start)
 t.start()
